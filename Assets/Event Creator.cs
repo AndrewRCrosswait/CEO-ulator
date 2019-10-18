fc37e195b509b7768ca5435cfade607c55a313b8
James Ferry <universalcoder8@gmail.com>
2019-10-18 18:52:26 -0400

    I hope this works

---
+++ Assets/Event Creator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventMaker : ScriptableObject
+{
+    public string name;
+    public string description;
+
+
+    public Sprite artwork;
+    public int deltaMD;
+    public int deltaPol;
+    public int deltaWC;
+    public int deltaMC;
+    public int deltaRE;
+    public int deltaGW;
+    // test comment
+}
