905ac9d4deaa36348a1b79dde7ab3e8662497bec
Ruel Coy <ruey01@gmail.com>
2019-10-18 18:10:03 -0400

    Added more to template and began work on the Event script. It will be used as the base of event creation

---
+++ Assets/Scripts/Events.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+// Template for Events in game.
+public class Events : ScriptableObject
+{
+    public string EventName;
+    public string EventDescription;
+
+    public Sprite sprite;
+
+    public int Money; // For both cost and gain
+}
