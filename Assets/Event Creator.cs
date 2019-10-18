18f09f40daba760c0d2f64c7ff7f980334084077
James Ferry <universalcoder8@gmail.com>
2019-10-18 19:10:31 -0400

    Prototype for Event implemented, can now add event in assets pannel

--- Assets/Event Creator.cs
+++ Assets/Event Creator.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="New Event", menuName ="Event")]
 public class EventMaker : ScriptableObject
 {
-    public string name;
+    public new string name;
     public string description;
 
 
     public Sprite artwork;
+
+    // Hidden values for change of industry rates
     public int deltaMD;
     public int deltaPol;
     public int deltaWC;
     public int deltaMC;
     public int deltaRE;
+
+
+    // Goodwill modifier
     public int deltaGW;
     // test comment
 }
